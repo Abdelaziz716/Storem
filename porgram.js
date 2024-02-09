66166bee568aaef4ddc1116e8b5267b7d9d22758
Abdelaziz716 <156494564+Abdelaziz716@users.noreply.github.com>
2024-02-09 09:51:03 +0300

    Add files via upload

---
+++ porgram.js
@@ -0,0 +1,4 @@
+const service =document.links("how to find")
+const project =console.error("you can't be hear at the moment")
+console.log("projecst num1")
+const storem =document.write("how to find the new project")
